aaed66652fb4c79c7f52b74f9264ecb8c01c95a0
okada <atunori06@gmail.com>
2021-01-24 17:56:47 +0900

    aaa

---
+++ db-pj/app/Http/Controllers/DbclassController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Contact;
+use Illuminate\Http\Request;
+
+class DbclassController extends Controller
+{
+   /** 
+    *Display a listing of the rsource.
+    *
+    *@return \Illuminate\Http\Response
+   */  
+  public function index()
+  {
+      $items = Contact::all();
+      return response()->json([
+          'message' => 'OK',
+          'data' => $items
+      ], 200);
+  }
+
+  /**
+   * Store a newly created resource in storage.
+   * 
+   * @param \Illuminate\Http\Request $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(Request $request)
+  {
+      $item = new Contact;
+      $item->name = $request->name;
+      $item->email = $request->email;
+      $item->save();
+      return response()->json([
+          'message' => 'Created successfully',
+          'data' => $item
+      ], 200);
+  }
+
+  /**
+   * Display the specified resource.
+   * 
+   * @param \App\Models\Contact $contact
+   * @return \Illuminate\Http\Response
+   */
+  public function update(Request $request, Contact $contact)
+  {
+      $item = Contact::where('id', $contact->id)->first();
+      $item->name = $request->name;
+      $item->email = $request->email;
+      $item->save();
+      if ($item) {
+          return response()->json([
+            'message' => $item,
+          ], 200);  
+      } else {
+          return response()->json([
+              'message' => 'Not found',
+          ], 404);
+      }
+  }
+
+  /**
+   * Remove the specified resouce from storage.
+   * 
+   * @param \App\Models\Contact $contact
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy(Contact $contact)
+  {
+      $item = Contact::where('id', $contact->id)->delete();
+      if ($item) {
+          return response()->json([
+              'message' => $item,
+          ], 200);
+      } else {
+          return response()->json([
+              'message' => $item,
+          ], 404);
+      }
+  }
+}
